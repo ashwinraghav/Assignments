aba9baa1d41eff953b699f81e1c1b1fb3fdc2d53
Ashwin <am2qa@virginia.edu>
2011-09-09 11:40:23 -0400

    The next time you are tired, sleep. Done in an hour in the morning

--- pl/hw1/hw1.ml
+++ pl/hw1/hw1.ml
@@ -29,8 +29,12 @@ let initial_state () : state = Hashtbl.create 255
 
 (* Given a state sigma, return the current value associated with
  * 'variable'. For our purposes all uninitialized variables start at 0. *)
-let insert (sigma:state) (variable:loc) (a:aexp) =
-    Hashtbl.add sigma variable 1
+let insert (sigma:state) (variable:loc) (a) : state =
+    Hashtbl.add sigma variable a ;sigma
+
+let remove (sigma:state) (variable:loc) : state =
+    Hashtbl.remove sigma variable ;sigma
+
 let lookup (sigma:state) (variable:loc) : n = 
   try
     Hashtbl.find sigma variable 
@@ -41,34 +45,41 @@ let lookup (sigma:state) (variable:loc) : n =
 let rec eval_aexp (a:aexp) (sigma:state) : n = match a with
   | Const(n) -> n
   | Var(loc) -> lookup sigma loc 
-  | Add(a0,a1) -> 
-    let n0 = eval_aexp a0 sigma in
-    let n1 = eval_aexp a1 sigma in
-    n0 + n1
-  | Sub(a0,a1) -> 
-    let n0 = eval_aexp a0 sigma in
-    let n1 = eval_aexp a1 sigma in
-    n0 - n1
-  | Mul(a0,a1) -> 
-    let n0 = eval_aexp a0 sigma in
-    let n1 = eval_aexp a1 sigma in
-    n0 * n1
+  | Add(a0,a1) -> eval_aexp a0 sigma + eval_aexp a1 sigma
+  | Sub(a0,a1) -> eval_aexp a0 sigma - eval_aexp a1 sigma
+  | Mul(a0,a1) -> eval_aexp a0 sigma * eval_aexp a1 sigma
 
 (* Evaluates a bexp given the state 'sigma'. *) 
 let rec eval_bexp (b:bexp) (sigma:state) : t = match b with
-  | True -> true
-  | False -> false 
-  | _ -> 
-    (* you must put real code here *) 
-    Printf.printf "Warning! BExp not yet implemented!\n" ; true 
+  | True              -> true
+  | False             -> false
+  | EQ(aexp1, aexp2)  -> eval_aexp aexp1 sigma= eval_aexp aexp2 sigma
+  | LE(aexp1, aexp2) -> eval_aexp aexp1 sigma <= eval_aexp aexp2 sigma
+  | Not bexp          -> not (eval_bexp bexp sigma)
+  | And(bexp1, bexp2) -> eval_bexp bexp1 sigma && eval_bexp bexp2 sigma
+  | Or(bexp1, bexp2)  -> eval_bexp bexp1 sigma || eval_bexp bexp2 sigma
 
 (* Evaluates a com given the state 'sigma'. *) 
 let rec eval_com (c:com) (sigma:state) : state = match c with
   | Skip -> sigma
-  | Print (a:aexp) -> 
-	let value = eval_aexp a sigma in 
-	Printf.printf "%d" value;sigma
-  | Set (loc, a) ->
-	Printf.printf "as";sigma
+  | Set(id, aexp) ->
+	let value = eval_aexp aexp sigma in
+	insert sigma id value;sigma
+  | Seq(com1, com2)  -> eval_com com2 (eval_com com1 sigma)
+  | If(bexp, com1, com2) ->
+      if eval_bexp bexp sigma then eval_com com1 sigma else eval_com com2 sigma
+  | While(bexp, com) ->
+      let rec loop sigma' bexp' com' =
+        if eval_bexp bexp' sigma' then loop (eval_com com' sigma') bexp' com'
+        else sigma' in
+      loop sigma bexp com
+  | Print (a:aexp) ->
+        let value = eval_aexp a sigma in
+        Printf.printf "%d" value;sigma
+  | Let (loc, aexp, com) ->
+        let value = eval_aexp aexp sigma in
+	insert sigma loc value;
+	eval_com com sigma;
+	remove sigma loc; sigma
   | _->
 	Printf.printf "not yet implemented";sigma
