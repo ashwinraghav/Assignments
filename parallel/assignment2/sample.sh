042a89fa9a6ebf3c3b7e297550d603a51d8afe6b
Ashwin <am2qa@virginia.edu>
2011-09-12 00:16:13 -0400

    Changing configs of master spawner

--- parallel/assignment2/sample.sh
+++ parallel/assignment2/sample.sh
@@ -1,14 +1,15 @@
-NODES=250
+NODES=8
 
 if [ "$PBS_ENVIRONMENT" != "" ] ; then
     echo Ack!  Launched via a qsub -- not continuing
     exit
 fi
 
+rm -rf output-*
+
 for (( i = 1; i <= $NODES; i++ )); do
-	echo "Creating job for node $i..."
-	rm -rf output-$i
-	PBS_DIR="output-$i"
+	#echo "Creating job for node $i..."
+	PBS_DIR="doutput-$i"
 	PBS_FILE="$PBS_DIR/job-$i.pbs"
 	#PBS_FILE="job-$i.pbs"
 	`mkdir $PBS_DIR`
@@ -24,13 +25,13 @@ for (( i = 1; i <= $NODES; i++ )); do
 done
 
 for (( i = 1; i <= 250; i++ )); do
-	PBS_DIR="output-$(($i % $NODES + 1))"
+	PBS_DIR="doutput-$(($i % $NODES + 1))"
 	PBS_FILE="$PBS_DIR/job-$(($i % $NODES + 1)).pbs"
 	echo "/usr/bin/blender -b ../Star-collapse-ntsc.blend -s $i -e $i -a" >> $PBS_FILE
 done
 
 for (( i = 1; i <= $NODES; i++ )); do
-	PBS_DIR="output-$i"
+	PBS_DIR="doutput-$i"
 	PBS_FILE="job-$i.pbs"
 	#PBS_FILE="job-$i.pbs"
 	cd $PBS_DIR
@@ -38,13 +39,12 @@ for (( i = 1; i <= $NODES; i++ )); do
 	cd ..
 done
 
-
 FRAME_COUNT=$((250/$NODES))
 
 START=$(date +%s)
 for (( i = 1; i <= $NODES; i++ )); do
 	while true; do
-		DONE_COUNT=`ls output-$i/star-collapse-* 2> NUL |wc -l`
+		DONE_COUNT=`ls doutput-$i/star-collapse-* 2> NUL |wc -l`
 		if (($DONE_COUNT >= $FRAME_COUNT)); then echo "DONE!!"; break; fi
 		#if diff -b -w test_result test_answer > NUL ; then echo "*** done" ; break; fi 
 	done
