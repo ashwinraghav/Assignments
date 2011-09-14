782fdbd115677adae8f95ab4e156d5ee673e6768
Ashwin <am2qa@virginia.edu>
2011-09-14 01:18:12 -0400

    Commiting with variable nodes

--- parallel/assignment2/sample.sh
+++ parallel/assignment2/sample.sh
@@ -1,4 +1,4 @@
-NODES=8
+NODES=100
 
 if [ "$PBS_ENVIRONMENT" != "" ] ; then
     echo Ack!  Launched via a qsub -- not continuing
@@ -6,14 +6,10 @@ if [ "$PBS_ENVIRONMENT" != "" ] ; then
 fi
 
 rm -rf output-*
+rm star-*
 
 for (( i = 1; i <= $NODES; i++ )); do
-	#echo "Creating job for node $i..."
-	#PBS_DIR="output-$i"
-	#PBS_FILE="$PBS_DIR/job-$i.pbs"
 	PBS_FILE="job-$i.pbs"
-	#PBS_FILE="job-$i.pbs"
-	#`mkdir $PBS_DIR`
 	`touch $PBS_FILE`
 	echo "#!/bin/sh" >  $PBS_FILE
 	echo "#PBS -l nodes=1:ppn=1" >>  $PBS_FILE
@@ -23,44 +19,30 @@ for (( i = 1; i <= $NODES; i++ )); do
 	echo "#PBS -m ea" >>  $PBS_FILE
 	echo "#PBS -M am2qa@virginia.edu" >>  $PBS_FILE
 	echo "cd \$PBS_O_WORKDIR" >>  $PBS_FILE
+	echo "cp /usr/bin/blender \$TMPDIR" >> $PBS_FILE
+	echo "cp Star-collapse-ntsc.blend \$TMPDIR" >> $PBS_FILE
 done
 
 for (( i = 1; i <= 250; i++ )); do
-	#PBS_DIR="output-$((($i % $NODES)  + 1))"
-	#PBS_FILE="$PBS_DIR/job-$(($i % $NODES + 1)).pbs"
 	PBS_FILE="job-$(($i % $NODES + 1)).pbs"
-	#echo "/usr/bin/blender -b ../Star-collapse-ntsc.blend -s $i -e $i -a" >> $PBS_FILE
-	echo "/usr/bin/blender -b Star-collapse-ntsc.blend -s $i -e $i -a" >> $PBS_FILE
+	echo "\$TMPDIR/blender -b \$TMPDIR/Star-collapse-ntsc.blend -s $i -e $i -a" >> $PBS_FILE
 done
 
 for (( i = 1; i <= $NODES; i++ )); do
-	#PBS_DIR="output-$i"
 	PBS_FILE="job-$i.pbs"
-	#PBS_FILE="job-$i.pbs"
-	#cd $PBS_DIR
 	qsub $PBS_FILE
-	#cd ..
 done
 
-#FRAME_COUNT=$((250/$NODES))
 FRAME_COUNT=250
 
+
 START=$(date +%s)
-#for (( i = 1; i <= $NODES; i=$i+2 )); do
 	while true; do
-		#DONE_COUNT=`ls output-$i/star-collapse-* 2> NUL |wc -l`
 		DONE_COUNT=`ls star-collapse-* 2> NUL |wc -l`
 		if (($DONE_COUNT >= $FRAME_COUNT)); then echo "DONE!!"; break; fi
-		#if diff -b -w test_result test_answer > NUL ; then echo "*** done" ; break; fi 
 	done
 #done
 END=$(date +%s)
 DIFF=$(( $END - $START ))
 echo "It took $DIFF seconds wall time"
-# note that there is another format for for loops in bash:
-#
-# for i in cheese apple crackers
-# do
-#   echo $i
-# done
 
