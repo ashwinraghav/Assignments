74a5cc2dc97947c5796d862aad762fe74c758cc5
Ashwin <am2qa@virginia.edu>
2011-09-25 15:53:00 -0400

    First meaningful commit on hw3

--- pl/hw3/hw3.ml
+++ pl/hw3/hw3.ml
@@ -1,18 +1,8 @@
-(* Graduate Programming Languages - Wes Weimer
- * 
- * YOUR NAME HERE (write your name here so that if the files get mixed up
- * somehow I can still give you credit for your work)
- *  
- * Edit this file according to the instructions in the homework and then
- * submit a renamed copy of it. Name the copy "uva_email_addr-hw3.ml". For
- * example, if you are Wes Weimer, send in "wrw6y-hw3.ml". 
- *)
-
-(*
- * Put the code for your interpreter in this file. Your interpreter should
- * be based on the denotational semantics for Regular Expressions that 
- * you developed in the homework. 
- *)
+(* 
+   Graduate Programming Languages - Wes Weimer
+   ASHWIN RAGHAV MOHAN GANESH
+   AM2QA
+*)
 
 open Re (* re.ml has the definitions for our regexp datatype *) 
 
@@ -21,19 +11,50 @@ open Re (* re.ml has the definitions for our regexp datatype *)
  * 'x''y' = str and re matches 'x'. Thus if re does not match any prefix 
  * of 'str' it should return the emptyset. 
  *)
+
 let rec matches (re : re) (s : re_string) : stringset = match re with
   | Empty -> singleton s 
   | Char(c) -> begin
     match s with
-    | c' :: rest when c = c' -> singleton rest 
+    | c' :: rest when c = c' ->  singleton rest 
     | _ -> emptyset 
   end 
-  | Question(re) -> matches (Or(re,Empty)) s
+  | Question(re) -> 
+	matches (Or(re,Empty)) s
+  | AnyChar -> begin
+    match s with	
+    | c1 :: rest -> singleton rest
+    | _ -> emptyset
+  end
 
-  (* you must fill in the other cases here *)
-
-  (* make sure that your program does not loop forever! *) 
-
+  | CharRange(from_char, to_char) -> begin
+    match s with	
+    | c1 :: rest ->
+      begin
+      match c1 with
+      |'a'..'z' -> singleton rest
+      | _ -> emptyset
+      end
+    | _ -> emptyset
+  end
+  | Concat(re1, re2) -> begin
+    let set1 = matches re1 s in
+    if is_empty set1 then
+	begin
+	emptyset
+	end
+    else
+	begin
+		let final_string = Re.fold (fun elt a -> a^Re.re_string_to_str elt) set1 "" in
+		matches re2 (string_to_re_string final_string)
+	end
+  end
+  | Or (re1,re2) -> begin
+    union (matches re1 s) (matches re2 s);
+  end
+  (*| Star(re)
+  | Plus(re)
+*)
   | _ -> Printf.printf "Error: RE %s unimplemented!\n"
     (re_to_str re) ; exit 1 
 
