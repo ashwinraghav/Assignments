ba4ee21153425cf524f7442758dfdd65165bed89
Ashwin <am2qa@virginia.edu>
2011-09-25 22:06:35 -0400

    Done baby

--- pl/hw3/hw3.ml
+++ pl/hw3/hw3.ml
@@ -1,16 +1,9 @@
 (* 
-   Graduate Programming Languages - Wes Weimer
    ASHWIN RAGHAV MOHAN GANESH
    AM2QA
 *)
 
-open Re (* re.ml has the definitions for our regexp datatype *) 
-
-(* 
- * "matches re str" returns a full set of suffices 'y' such that
- * 'x''y' = str and re matches 'x'. Thus if re does not match any prefix 
- * of 'str' it should return the emptyset. 
- *)
+open Re 
 
 let rec matches (re : re) (s : re_string) : stringset = match re with
   | Empty -> singleton s 
@@ -23,43 +16,45 @@ let rec matches (re : re) (s : re_string) : stringset = match re with
 	matches (Or(re,Empty)) s
   | AnyChar -> begin
     match s with	
-    | c1 :: rest -> singleton rest
+    | c1 :: rest -> matches(Char(c1)) s
     | _ -> emptyset
   end
 
   | CharRange(from_char, to_char) -> begin
     match s with	
+    (*char range c1..c2 was painful. Could not get it to work. Hence the ugly loop*)
     | c1 :: rest ->
-      begin
-      match c1 with
-      |'a'..'z' -> singleton rest
-      | _ -> emptyset
-      end
+      let rec loop from_c to_c = let ascii1 = Char.code from_c in let ascii2= Char.code to_c in
+      if (ascii1 > ascii2) then
+	emptyset
+      else
+        let set1 = (matches (Char(from_c)) s)in
+        if is_empty set1 then
+          loop (Char.chr (ascii1+1)) to_c
+        else
+          set1;
+      in loop from_char to_char 
     | _ -> emptyset
   end
   | Concat(re1, re2) -> begin
-    Printf.printf "re1 %s" (re_to_str re1);
+    (*Printf.printf "re1 %s" (re_to_str re1);
     Printf.printf "re2 %s" (re_to_str re2);
-    Printf.printf "string %s" (re_string_to_str s);
+    Printf.printf "string %s" (re_string_to_str s); *)
     let set1 = matches re1 s in
     if is_empty set1 then
 	emptyset
     else
 	begin
-    	Printf.printf "re1 is not empty";
+    	(*Printf.printf "re1 is not empty";*)
 	let final_string = Re.fold (fun elt a -> a^Re.re_string_to_str elt) set1 "" in
 	matches re2 (string_to_re_string final_string)
 	end
   end
-  | Or (re1,re2) -> begin
+  | Or (re1,re2) ->
     union (matches re1 s) (matches re2 s);
-  end
   | Star(re) ->
-		union (singleton s)(matches (Concat(re,Star(re))) s);
-(*
-  | Plus(re)
-*)
+    union (singleton s)(matches (Concat(re,Star(re))) s);
+  | Plus(re)->
+    matches (Concat(re,Star(re))) s;	
   | _ -> Printf.printf "Error: RE %s unimplemented!\n"
     (re_to_str re) ; exit 1 
-
-
